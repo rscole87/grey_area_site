39274f719cfc573bc5b18e61bb3b988b76da1b23
Rodrick Coleman <rscole87@gmail.com>
2021-07-13 23:37:49 -0400

    add styling for section bgs, navbar, and font colors

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -11,7 +11,7 @@ export default function Header() {
 
   return (
     <>
-      <Navbar light sticky="top" expand="md">
+      <Navbar dark sticky="top" expand="md" style={{ backgroundColor: "#128df1bd" }}>
         <div className="container">
           <NavbarBrand className="mr-auto" href="/">
             <img src="" height="30" width="30" alt="Grey Area Services" />
@@ -50,18 +50,20 @@ export default function Header() {
       <section id="header-section">
         <Container>
           <Row id="header">
-            <Col>
-              <h1>It's all about the Grey</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-              <div className="row">
-                <div className="col-3"></div>
-                <div className="col-3">
-                  <button className="input-bttn ">View More</button>
+            <Col id="header-content-container" className="mx-auto my-auto">
+              <div id="header-content">
+                <h1>It's all about the Grey</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                <div className="row">
+                  <div className="col-3"></div>
+                  <div className="col-3">
+                    <button className="input-bttn ">View More</button>
+                  </div>
+                  <div className="col-3">
+                    <button className="input-bttn mx-auto">Get Started</button>
+                  </div>
+                  <div className="col-3"></div>
                 </div>
-                <div className="col-3">
-                  <button className="input-bttn mx-auto">Get Started</button>
-                </div>
-                <div className="col-3"></div>
               </div>
             </Col>
           </Row>
