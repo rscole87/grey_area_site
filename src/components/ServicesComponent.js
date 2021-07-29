16d7fc596e1a4de870d1895df48b5e219b286d57
Rodrick Coleman <rscole87@gmail.com>
2021-07-29 16:48:55 -0400

    add styling to services list

--- src/components/ServicesComponent.js
+++ src/components/ServicesComponent.js
@@ -32,78 +32,93 @@ export default function Services() {
               <h4 className="service-heading">Security Services</h4>
               <p className="service-description">If you have concerns about your physical safety or the safety of high value items, our military-trained team is available to help put your mind at ease.</p>
             </Col>
-            <Row>
-              <Col className="d-flex justify-content-center">
-                <button className="input-bttn" onClick={() => toggleServicesList()}>{isOpen ? 'Hide' : 'Show'} all services</button>
-              </Col>
-            </Row>
           </Row>
         </Container>
 
-        <Collapse id="services-list-div" isOpen={isOpen}>
+        <Container>
+          <Collapse id="services-list-div" className="p-5" isOpen={isOpen}>
+            <h6>Investigation and Business Intelligence Services</h6>
+            <ol>
+              <li>Process Serving</li>
+              <li>Background Checks</li>
+              <li>
+                Litigation
+                <ul>
+                  <li>Child Custody</li>
+                  <li>Infidelity/Divorce</li>
+                  <li>Claims Against Law Enforcement</li>
+                </ul>
+              </li>
+              <li>Insurance – Full Suite of Services</li>
+
+              <li>
+                Support to Corporate Security and Legal Departments
+                <ul>
+                  <li>Compliance assessments</li>
+                  <li>Threat Assessments</li>
+                  <li>Vulnerability Assessments</li>
+                  <li>
+                    Security Assessment (Physical)
+                    <ul>
+                      <li>Scalable and flexible</li>
+                      <li>Personnel, systems, and procedures</li>
+                      <li>Announced or unannounced</li>
+                      <li>Overt or discreet presence and activity</li>
+                    </ul>
+                  </li>
+                  <li>
+                    Internal Investigations
+                    <ul>
+                      <li>Physical Security Breach</li>
+                      <li>Theft</li>
+                      <li>Damage to property</li>
+                      <li>Employee misconduct</li>
+                      <li>Sexual harassment</li>
+                      <li>Discrimination</li>
+                    </ul>
+                  </li>
+                  <li>Due diligence to support mergers, acquisitions, and sub-contracting</li>
+                </ul>
+              </li>
+            </ol>
+
+            <h6>Security Services</h6>
+            <ol>
+              <li>Specializing in low visibility or discreet activity</li>
+              <li>VIP, executive protection, and personal security - Armed or unarmed</li>
+              <li>High value item protection and transport</li>
+              <li>Sobriety Supervisors, Sobriety Chaperones, Sober Companions, or Sober Escorts</li>
+            </ol>
+
+            <h6>Our Approach</h6>
+            <ol>
+              <li>Discreet surveillance – Digital and physical</li>
+              <li>
+                Site/scene forensics, documentation, and analysis
+                <ul>
+                  <li>Law enforcement ready reports</li>
+                  <li>Photo and video recording</li>
+                  <li>2D and 3D digital modeling</li>
+                </ul>
+              </li>
+              <li>UAV (Drone) support with licensed drone pilots</li>
+              <li>Polygraph – in-house certified polygrapher</li>
+              <li>Electronic data recovery</li>
+              <li>Video and audio analysis</li>
+              <li>International travel ready</li>
+              <li>Foreign language proficiency</li>
+            </ol>
+          </Collapse>
           <Row>
-            <Col sm="6" lg="4" className="order-sm-1 order-lg-1">
-              <h6>Investigation and Business Intelligence Services</h6>
-              <ul>
-                <li>Process Serving</li>
-                <li>Background Checks</li>
-                <li>
-                  Litigation
-                  <ol>
-                    <li>Child Custody</li>
-                    <li>Infidelity/Divorce</li>
-                    <li>Claims Against Law Enforcement</li>
-                  </ol>
-                </li>
-                <li>Insurance – Full Suite of Services</li>
-              </ul>
-            </Col>
-
-            <Col sm="12" lg="4" className="order-sm-3 order-lg-2">
-              <ul>
-                <li>
-                  Support to Corporate Security and Legal Departments
-                  <ol>
-                    <li>Compliance assessments</li>
-                    <li>Threat Assessments</li>
-                    <li>Vulnerability Assessments</li>
-                    <li>
-                      Security Assessment (Physical)
-                      <ul>
-                        <li>Scalable and flexible</li>
-                        <li>Personnel, systems, and procedures</li>
-                        <li>Announced or unannounced</li>
-                        <li>Overt or discreet presence and activity</li>
-                      </ul>
-                    </li>
-                    <li>
-                      Internal Investigations
-                      <ul>
-                        <li>Physical Security Breach</li>
-                        <li>Theft</li>
-                        <li>Damage to property</li>
-                        <li>Employee misconduct</li>
-                        <li>Sexual harassment</li>
-                        <li>Discrimination</li>
-                      </ul>
-                    </li>
-                    <li>Due diligence to support mergers, acquisitions, and sub-contracting</li>
-                  </ol>
-                </li>
-              </ul>
-            </Col>
-
-            <Col sm="6" lg="4" className="order-sm-2 order-lg-3">
-              <h6>Security Services</h6>
-              <ul>
-                <li>Specializing in low visibility or discreet activity</li>
-                <li>VIP, executive protection, and personal security - Armed or unarmed</li>
-                <li>High value item protection and transport</li>
-                <li>Sobriety Supervisors, Sobriety Chaperones, Sober Companions, or Sober Escorts</li>
-              </ul>
+            <Col className="d-flex justify-content-center">
+              <div className="mx-auto">
+                <button className="input-bttn" onClick={() => toggleServicesList()}>
+                  {isOpen ? "Hide" : "Show"} all services
+                </button>
+              </div>
             </Col>
           </Row>
-        </Collapse>
+        </Container>
       </section>
     </>
   );
